7a7c2d1e61719b0e571cb7e85c9119b904d16142
Patryk Kasztelan <patryk@ark-software.com>
2025-01-22 21:57:31 +0100

    Add classes screen content

--- app/src/main/java/com/example/dancebuddy/screens/ScreenBases.kt
+++ app/src/main/java/com/example/dancebuddy/screens/ScreenBases.kt
@@ -3,28 +3,41 @@ package com.example.dancebuddy.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
+import com.example.dancebuddy.coredata.DanceClass
 import com.example.dancebuddy.coredata.user.MainUser
+import com.example.dancebuddy.coredata.viewModels.DanceClassViewModel
 import com.example.dancebuddy.coredata.viewModels.EventViewModel
+import com.example.dancebuddy.screens.components.DanceClassModal
+import com.example.dancebuddy.screens.components.DanceClassSection
 import com.example.dancebuddy.screens.components.EmptyEvents
 import com.example.dancebuddy.screens.components.EventCarousel
 import com.example.dancebuddy.screens.components.LoginCard
 import com.example.dancebuddy.screens.components.ProfileCard
 import com.example.dancebuddy.screens.components.Section
+import com.example.dancebuddy.screens.components.SectionHeader
 
 @Composable
 fun HomeScreen(innerPadding: PaddingValues, navController: NavHostController) {
@@ -43,12 +56,83 @@ fun HomeScreen(innerPadding: PaddingValues, navController: NavHostController) {
 
 @Composable
 fun ClassScreen(innerPadding: PaddingValues, navController: NavHostController) {
-    Column(
-        modifier = Modifier
-            .padding(innerPadding)
-            .fillMaxWidth()
-            .wrapContentHeight()
-    ) {
+    val danceClassViewModel: DanceClassViewModel = viewModel()
+    val danceClasses by danceClassViewModel.data.observeAsState(initial = emptyList())
+    val filteredClasses = danceClasses.filter { danceClass ->
+        danceClass.users.any { user -> user.id == MainUser.id }
+    }
+    val isLoading by danceClassViewModel.loading.observeAsState(initial = false)
+    var selectedDanceClass by remember { mutableStateOf<DanceClass?>(null) }
+
+    if (isLoading) {
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .size(50.dp)
+            )
+        }
+    } else if (danceClasses.isEmpty()) {
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                color = Color.Gray,
+                text = "No classes."
+            )
+        }
+    } else {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            item {
+                SectionHeader("Your classes")
+            }
+            item {
+                DanceClassSection(
+                    innerPadding = PaddingValues(16.dp),
+                    danceClasses = filteredClasses,
+                    onClassClick = { selectedDanceClass = it }
+                )
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+
+            item {
+                SectionHeader("All classes")
+            }
+            item {
+                DanceClassSection(
+                    innerPadding = PaddingValues(16.dp),
+                    danceClasses = danceClasses,
+                    onClassClick = { selectedDanceClass = it }
+                )
+            }
+
+            item {
+                selectedDanceClass?.let { danceClass ->
+                    DanceClassModal(
+                        danceClass = danceClass,
+                        onDismiss = { selectedDanceClass = null }
+                    )
+                }
+            }
+        }
+
     }
 }
 
--- app/src/main/java/com/example/dancebuddy/screens/components/ClassComponents.kt
+++ app/src/main/java/com/example/dancebuddy/screens/components/ClassComponents.kt
@@ -1,5 +1,110 @@
 package com.example.dancebuddy.screens.components
 
-//TODO: your classes
-//TODO: all classes
-//TODO: onClick class - dialog with description (without registration)
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.dancebuddy.coredata.DanceClass
+
+@OptIn(ExperimentalLayoutApi::class)
+@Composable
+fun DanceClassSection(
+    innerPadding: PaddingValues,
+    danceClasses: List<DanceClass>,
+    onClassClick: (DanceClass) -> Unit
+) {
+    BoxWithConstraints(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(innerPadding)
+    ) {
+        val cardWidth = maxWidth / 2 - 16.dp
+        FlowRow(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            danceClasses.forEach { danceClass ->
+                DanceClassCard(
+                    modifier = Modifier
+                        .width(cardWidth)
+                        .wrapContentHeight(),
+                    danceClass = danceClass,
+                    onClick = { onClassClick(danceClass) }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DanceClassCard(modifier: Modifier = Modifier, danceClass: DanceClass, onClick: () -> Unit) {
+    Card(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(vertical = 8.dp),
+        shape = RoundedCornerShape(8.dp),
+        elevation = CardDefaults.cardElevation(4.dp),
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            Text(
+                text = danceClass.title,
+                style = MaterialTheme.typography.headlineMedium
+            )
+            Text(
+                text = "Level: ${danceClass.level}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+            Text(
+                text = "Participants: ${danceClass.users.size}",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
+    }
+}
+
+@Composable
+fun DanceClassModal(danceClass: DanceClass, onDismiss: () -> Unit) {
+    AlertDialog(
+        onDismissRequest = onDismiss,
+        title = {
+            Text(text = danceClass.title)
+        },
+        text = {
+            Column {
+                Text("Level: ${danceClass.level}")
+                Text("Time: ${danceClass.time}")
+                Text("Days: ${danceClass.days.joinToString(", ")}")
+                Text("Description: ${danceClass.description}")
+                Text("Participants:")
+                danceClass.users.forEach { user ->
+                    Text("- ${user.firstName} ${user.lastName}")
+                }
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDismiss) {
+                Text("Close")
+            }
+        }
+    )
+}
